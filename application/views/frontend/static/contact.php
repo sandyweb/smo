5b64af53be4e54d78a87da3046e0ed641a53094e
Ievgen <jonik.lowkick@gmail.com>
2013-07-02 16:01:07 +0300

    Changed contact view. Added email template

--- application/views/frontend/static/contact.php
+++ application/views/frontend/static/contact.php
@@ -4,54 +4,57 @@
         <h2 class="title blue">Contact Us</h2>
         <!-- start grid_9 -->
         <section class="grid_9">
-            <!-- start fillform -->
-            <form method="post" action="contact" id="signupform" class="fillform blue2">
-                <div class="aspNetHidden">
-                    <input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwULLTE0MTkwMzcyMTAPZBYCZg9kFgICAQ9kFgJmD2QWAgIFDw8WAh4HVmlzaWJsZWhkZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WAwUkY3RsMDAkQ29udGVudFBsYWNlSG9sZGVyMSRyZEN1c3RvbWVyBSZjdGwwMCRDb250ZW50UGxhY2VIb2xkZXIxJHJkRnJlZWxhbmNlcgUhY3RsMDAkQ29udGVudFBsYWNlSG9sZGVyMSRyZE90aGVyVEBbajjkfYc5nEg+Ih6ECPTTjqJec+858lXamKU448Q=" />
-                </div>
-
-                <div class="aspNetHidden">
-                    <input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="/wEWCgKKuc6qCALK5azIAgKyxbzfCAK81I20AQK0w7i7DQLUnp3xAQKG0+i/BgK8/rr5DwKR/JOfDAKA+uPIBmwcMTlkZrKbtcH6MPpcnAKvRzNEhZ6wpp8QwP1ITalF" />
-                </div>
+            <?=Form::open(URL::site('main/contact'), array('method' => 'post', 'id' => 'signupform', 'class' => 'fillform blue2'));?>
+                <?php if($errors):?>
+                    <p class="message">Some errors were encountered, please check the details you entered.</p>
+                    <ul class="error">
+                    <?php foreach($errors as $message):?>
+                        <li><?=$message;?></li>
+                    <?php endforeach;?>
+                    </ul>
+                <?php elseif($message):?>
+                    <p class="message"><?=$message;?></p>
+                <?php endif;?>
+                <!-- start fillform -->
                 <div id="ContentPlaceHolder1_signupformDIV">
                     <section class="row" style="text-align:left">
                         <label>Type:</label>
-                        <label class="second" for="Customer"><input value="rdCustomer" name="ctl00$ContentPlaceHolder1$a" type="radio" id="ContentPlaceHolder1_rdCustomer" />Customer</label>
-                        <label class="second" for="Freelancer"><input value="rdFreelancer" name="ctl00$ContentPlaceHolder1$a" type="radio" id="ContentPlaceHolder1_rdFreelancer" />Freelancer</label>
-                        <label class="second" for="Other"><input value="rdOther" name="ctl00$ContentPlaceHolder1$a" type="radio" id="ContentPlaceHolder1_rdOther" />Other</label>
+                        <label class="second"><?=Form::radio('message[type]', 'Customer');?>Customer</label>
+                        <label class="second"><?=Form::radio('message[type]', 'Freelancer');?>Freelancer</label>
+                        <label class="second"><?=Form::radio('message[type]', 'Other');?>Other</label>
                         <section class="clear"></section>
                     </section>
                     <section class="row">
-                        <label for="Name2">Name:</label>
-                        <input name="ctl00$ContentPlaceHolder1$txtName" type="text" id="ContentPlaceHolder1_txtName" class="input1" />
+                        <label>Name:</label>
+                        <?=Form::input('message[name]', $data['name'], array('class' => 'input1'));?>
                         <section class="clear"></section>
                     </section>
                     <section class="row">
-                        <label for="Email2">Email:</label>
-                        <input name="ctl00$ContentPlaceHolder1$txtEmail" type="text" id="ContentPlaceHolder1_txtEmail" class="input1" />
+                        <label>Email:</label>
+                        <?=Form::input('message[email]', $data['email'], array('class' => 'input1'));?>
                         <section class="clear"></section>
                     </section>
                     <section class="row">
-                        <label for="Subject">Subject:</label>
-                        <input name="ctl00$ContentPlaceHolder1$txtSubject" type="text" id="ContentPlaceHolder1_txtSubject" class="input1" />
+                        <label>Subject:</label>
+                        <?=Form::input('message[subject]', $data['subject'], array('class' => 'input1'));?>
                         <section class="clear"></section>
                     </section>
                     <section class="row">
-                        <label for="Job">Project # <span>(optional):</span></label>
-                        <input name="ctl00$ContentPlaceHolder1$txtJob" type="text" id="ContentPlaceHolder1_txtJob" class="input1" />
+                        <label>Project # <span>(optional):</span></label>
+                        <?=Form::input('message[project]', $data['project'], array('class' => 'input1'));?>
                         <section class="clear"></section>
                     </section>
                     <section class="row second">
-                        <label for="Message">Message: </label>
-                        <textarea name="ctl00$ContentPlaceHolder1$txtMessage" id="ContentPlaceHolder1_txtMessage" class="textarea1" cols="4" rows="4"></textarea>
+                        <label for="Message">Message:</label>
+                        <?=Form::textarea('message[message]', $data['message'], array('class' => 'textarea1', 'cols' => '4', 'rows' => '4'));?>
                         <section class="clear"></section>
                     </section>
                     <section class="row2">
-                        <input type="submit" name="ctl00$ContentPlaceHolder1$btnSendMessage" value="Send" id="ContentPlaceHolder1_btnSendMessage" class="bt-gray" />
+                        <?=Form::submit('send', 'Send', array('class' => 'bt-gray'));?>
                         <section class="clear"></section>
                     </section>
                 </div>
-            </form>
+            <?=Form::close();?>
             <section class='txtb'> </section>
             <!-- end fillform -->
             <section class="clear"></section>
@@ -72,9 +75,6 @@
                     <li class="rss"><a target="_blank" href="http://blog.ziptask.com/rss" title="Rss">Rss</a></li>
                 </ul>
                 <section class="clear"></section>
-                <!--
-                <a href="http://www.vocalocity.com/" target="Click_to_Call_Me" onclick='window.open("http://www.vocalocity.com/click2callme/?key=HKzrqxrjNGPFEAgSPca&company=Ziptask%2C%20Inc.", "Click_to_Call_Me", "resizable=no,width=460,height=200"); return false;'><img border="0" src="http://www.vocalocity.com/click2callme/images/c2cm-image1.png" alt="internet phone service"></a>
-                -->
                 <section class="clear"></section>
             </section>
             <!-- end address-block -->
---
+++ application/views/mails/frontend/contact.php
@@ -0,0 +1,15 @@
+<html>
+<head></head>
+<body>
+    <div>
+        <h3><?=$subject;?></h3>
+    </div>
+    <div>
+        <p>Type: <?=$type;?></p>
+        <p>Name: <?=$name;?></p>
+        <p>Email: <?=$email;?></p>
+        <p>Project#: <?=$project;?></p>
+        <p><?=$message;?></p>
+    </div>
+</body>
+</html>

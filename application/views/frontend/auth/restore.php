6938b00d92b303c46ee9b3c851c00cd802fa3ee8
Ievgen <jonik.lowkick@gmail.com>
2013-07-11 13:57:47 +0300

    Refactored auth views

--- application/views/frontend/auth/restore.php
+++ application/views/frontend/auth/restore.php
@@ -10,18 +10,14 @@
     <div class="logo"><?php // echo HTML::image("files/template/frontend/logo.png");?></div>
     <div class="login_wrapper">
         <?php echo Form::open();?>
-
             <?php echo Form::input("email", arr::get($form, 'email'), array("placeholder"=>"Email", "class"=>"input"));?>
-        
             <?php // if (isset(arr::get($errors, 'email'))):?>
                 <span class="redmessage"><?php echo arr::get($errors, 'email');?></span>
             <?php // endif;?>
-            
-            <?php echo Form::submit("restore", __("SEND PASSWORD"), array("class"=>"login-btn", "style"=>"height: 26px"));?>
-            
+            <?php echo Form::submit("restore", __("Send Password"), array("class"=>"login-btn", "style"=>"height: 26px"));?>
             <div class="actions_login">
                 <?php echo HTML::anchor('auth/login', __("Login"), array("title"=>"Login"));?>
-                <?php echo HTML::anchor('main/index', __("Home page"), array("title"=>"Learn More..."));?>
+                <?php echo HTML::anchor(URL::base(), __("Home page"), array("title"=>"Learn More..."));?>
             </div>
 
         <?php echo Form::close();?>

a1c8ef916ea1f237470c50c5d0fb9faf94b40415
joni <win7.vb.@test.com>
2013-07-18 17:48:18 +0300

    Changed view for message history

--- application/views/frontend/manager/message_history.php
+++ application/views/frontend/manager/message_history.php
@@ -1,35 +1,27 @@
-<table id="message-history-table">
-    <thead>
-    <tr>
-        <td></td>
-        <td>Date</td>
-        <td>Subject</td>
-        <td>Status</td>
-    </tr>
-    </thead>
-    <tbody>
+<div class="message-list-container">
     <?php foreach($messages as $message):?>
-        <tr>
-            <?php if($sender == $message->sender_id):?>
-                <td>Manager:</td>
-            <?php else:?>
-                <td>Client:</td>
-            <?php endif;?>
-            <td><?=Date::to_datetime($message->created);?></td>
-            <td><?=$message->subject;?></td>
-            <td><?=HTML::anchor(URL::site('manager/outbox_view/'.$message->id), __('Details'));?></td>
-        </tr>
+        <?php $class = ($sender == $message->sender_id) ? 'manager' : 'client';?>
+        <div class="message-container <?=$class;?>">
+            <div class="left-subcontainer">
+                <div class="message-user-avatar">
+                    <?php $image = $message->sender->image;?>
+                    <?php if(!$image):?>
+                        <?=HTML::image('files/template/frontend/pm-icon.png');?>
+                    <?php else :?>
+                        <?=HTML::image('files/media/avatars/'.$image);?>
+                    <?php endif;?>
+                </div>
+                <p><?=Date::to_datetime($message->created);?></p>
+            </div>
+            <div class="right-subcontainer">
+                <h4><?=$message->subject;?></h4>
+                <p><?=$message->message;?></p>
+            </div>
+            <div class="both">
+                <p><?=HTML::anchor(URL::site('manager/outbox_view/'.$message->id), __('Details'));?></p>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="clear"></div>
     <?php endforeach;?>
-    </tbody>
-</table>
-<script>
-    $(document).ready(function(){
-        $('#message-history-table').dataTable({
-            iDisplayLength: 20,
-            bFilter: false,
-            bInfo: false,
-            bLengthChange: false,
-            bSort: false
-        });
-    });
-</script>
+</div>

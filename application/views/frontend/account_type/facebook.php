69b980fb1ace168ea4e83b407211931b6ebebb1c
joni <win7.vb.@test.com>
2013-07-18 15:19:04 +0300

    Changed facebook account view

--- application/views/frontend/account_type/facebook.php
+++ application/views/frontend/account_type/facebook.php
@@ -1,3 +1,121 @@
 <div class="fb-container">
-
+    <div class="options-container">
+        <h4><?=__('Page Posting');?></h4>
+        <?php foreach($posting_range as $range):?>
+            <?php $selected = ($range->id == $default_posting_range) ? TRUE : FALSE;?>
+            <?=Form::radio('posting_range', $range->id, $selected, array('data-price' => $range->price));?>
+            <span><?=$range->name;?></span>
+        <?php endforeach;?>
+        <?=Form::input('posting_range_custom', '', array('class="input span2 hide"'));?>
+    </div>
+    <div class="options-container">
+        <h4><?=__('Comments Posting');?></h4>
+        <?php foreach($comments_range as $range):?>
+            <?php $selected = ($range->id == $default_comments_range) ? TRUE : FALSE;?>
+            <?=Form::radio('comments_range', $range->id, $selected, array('data-price' => $range->price));?>
+            <span><?=$range->name;?></span>
+        <?php endforeach;?>
+        <?=Form::input('comments_range_custom', '', array('class="input span2 hide"'));?>
+    </div>
+    <div class="options-container">
+        <h4><?=__('Like Posting');?></h4>
+        <?php foreach($like_range as $range):?>
+            <?php $selected = ($range->id == $default_like_range) ? TRUE : FALSE;?>
+            <?=Form::radio('like_range', $range->id, $selected, array('data-price' => $range->price));?>
+            <span><?=$range->name;?></span>
+        <?php endforeach;?>
+        <?=Form::input('like_range_custom', '', array('class="input span2 hide"'));?>
+    </div>
+    <div class="options-container">
+        <label><?=__('Total page likes');?></label>
+        <?=Form::input('total_page_likes', '', array('class="input span2"'));?>
+    </div>
+    <div class="options-container">
+        <h4><?=__('Required input information source');?></h4>
+        <?php foreach($sources as $source):?>
+            <?php $selected = ($source->id == $default_source) ? TRUE : FALSE;?>
+            <?=Form::radio('information_source', $source->id, $selected, array('data-price' => $source->price));?>
+            <span><?=$source->name;?></span>
+        <?php endforeach;?>
+    </div>
 </div>
+<script>
+    $(document).ready(function(){
+        var $posting_range_custom = $('input[name="posting_range_custom"]');
+        var $posting_range = $('input[name="posting_range"]');
+        $posting_range.change(function(){
+            if($(this).next().text() == 'Customs posts/month'){
+                $posting_range_custom.show()
+            }
+            else{
+                $posting_range_custom.hide();
+                $posting_range_custom.val('');
+                calculateSummary();
+            }
+        });
+
+        $posting_range_custom.change(function(){
+            calculateSummary();
+        });
+
+        var $comments_range_custom = $('input[name="comments_range_custom"]');
+        var $comments_range = $('input[name="comments_range"]');
+        $comments_range.change(function(){
+            if($(this).next().text() == 'Custom comments per post'){
+                $comments_range_custom.show()
+            }
+            else{
+                $comments_range_custom.hide();
+                $comments_range_custom.val('');
+                calculateSummary();
+            }
+        });
+
+        $comments_range_custom.change(function(){
+            calculateSummary();
+        });
+
+        var $like_range_custom = $('input[name="like_range_custom"]');
+        var $like_range = $('input[name="like_range"]');
+        $like_range.change(function(){
+            if($(this).next().text() == 'Custom like per post'){
+                $like_range_custom.show()
+            }
+            else{
+                $like_range_custom.hide();
+                $like_range_custom.val('');
+                calculateSummary();
+            }
+        });
+
+        $like_range_custom.change(function(){
+            calculateSummary();
+        });
+
+        var $information_source = $('input[name="information_source"]');
+        $information_source.change(function(){
+            calculateSummary();
+        });
+
+        function calculateSummary(){
+            var price = parseInt($('input[name="posting_range"]:checked').attr('data-price'));
+            if($posting_range_custom.val() != ''){
+                price *= parseInt($posting_range_custom.val());
+            }
+            price += parseInt($('input[name="comments_range"]:checked').attr('data-price'));
+            if($comments_range_custom.val() != ''){
+                price *= parseInt($comments_range_custom.val());
+            }
+
+            price += parseInt($('input[name="like_range"]:checked').attr('data-price'));
+            if($like_range_custom.val() != ''){
+                price *= parseInt($like_range_custom.val());
+            }
+
+            price += parseInt($('input[name="information_source"]:checked').attr('data-price'));
+            console.log(price);
+            $('.account-price').text('$' + convertCentsDollars(price));
+            $('input[name="price"]').val(price);
+        }
+    });
+</script>

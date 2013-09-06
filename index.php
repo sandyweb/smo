7017f8ecef1bcd091589378ada4547d63408c68f
AB <bashalex87@yahoo.com>
2013-09-06 14:06:04 +0300

    test commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+echo "TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<BR><BR>";
 /**
  * The directory in which your application specific resources are located.
  * The application directory must contain the bootstrap.php file.
